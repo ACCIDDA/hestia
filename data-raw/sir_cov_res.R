08a370266023ccfc5641934fa7d5c79a7a111ea1
Smith <cpsmit@AD.UNC.EDU>
2026-04-09 11:22:26 -0400

    Update SIR vignette to use pre-computed model outcomes included in the package data

---
+++ data-raw/sir_cov_res.R
@@ -0,0 +1,29 @@
+## code to prepare `DATASET` dataset goes here
+library(hestia)
+library(dplyr)
+
+# Basic SIR, fit recovery rate
+inf_process <- make_infection_model(
+  transmit(from = "S", to = "I"),
+  progress(from = "I", to = "R", gamma = NA)
+)
+
+# Observation process
+obs_process <- make_observation_model(
+  pcr = c("S" = 0.05, "I" = 0.95, "R" = 0.05),
+  igg = c("S" = 0.01, "I" = 0.01, "R" = 0.8)
+)
+
+# Run stan model on SIR package data
+sir_cov_res <- run_model(
+  inf_model = inf_process,
+  obs_model = obs_process,
+  data = sir_cov$observations,
+  ih_cov = sir_cov$covariates,
+  eh_cov = sir_cov$covariates,
+  init_probs = c(1 - 2 * 1e-10, 1e-10, 1e-10),
+  iter = 1000,
+  cores = 4
+)
+
+usethis::use_data(sir_cov_res, overwrite = TRUE)
