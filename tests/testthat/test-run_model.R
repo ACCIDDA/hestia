a7b8c2410bcb0c84ace1daa3392d9e2f5f837ee9
Smith <cpsmit@AD.UNC.EDU>
2026-09-14 15:43:50 -0400

    Fix input name for run_model tests

--- tests/testthat/test-run_model.R
+++ tests/testthat/test-run_model.R
@@ -489,7 +489,7 @@ test_that("State probabilities match expectation for toy examples", {
     ),
     progress(from = "Is", to = "R", gamma_s = NA),
     progress(from = "Ia", to = "R", gamma_a = NA),
-    mult_inf_probs = TRUE
+    mult_ih_inf_probs = TRUE
   )
 
   obs_process <- make_observation_model(
